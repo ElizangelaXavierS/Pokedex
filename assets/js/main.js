acbf022975ab7e8ef4f20344148cd5d8855f6c23
Elizangela Xavier <elizangelaxavier913@gmail.com>
2022-11-19 15:57:32 -0300

    Convertento o modelo do PokeApi para outro modelo.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,16 @@
 
-const offset = 0;
-const limit =10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 function convertePokemonEmLista(pokemon){
     return `
-         <li class="pokemon">
-            <span class="number">#001</span>
+         <li class="pokemon ${pokemon.type}" >
+            <span class="number">#${pokemon.number}</span>
              <span class="name">${pokemon.name}</span>
 
              <div class="detail">
                  <ol class="types">
-                     <li class="type">grass</li>
-                     <li class="type">poison</li>
+                     ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                   </ol>
-                 <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                  <img src="${pokemon.photo}"
                   alt="${pokemon.name}">
              </div>                        
         </li>
@@ -36,6 +32,6 @@ function convertePokemonEmLista(pokemon){
 
     // versão simplificada
     pokeApi.getPokemons().then((pokemons = []) => {
-        pokemonList.innerHTML += pokemons.map(convertePokemonEmLista).join('')
+        pokemonList.innerHTML = pokemons.map(convertePokemonEmLista).join('')
     })
    
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,10 +1,30 @@
 
 const pokeApi = {}
 
-pokeApi.getpokemonDetalhes = (pokemon) => {
-    return fetch(pokemon.url).then((response) => response.json())
+function convertPokeApiDetalhesEmPokemnon(pokeDetalhes){
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetalhes.order
+    pokemon.name = pokeDetalhes.name
+
+    const types = pokeDetalhes.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetalhes.sprites.other.dream_world.front_default
     
+    return pokemon
 }
+
+pokeApi.getpokemonDetalhes = (pokemon) => {
+    return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDetalhesEmPokemnon) 
+
+}
+    
+
 
 pokeApi.getPokemons = (offset = 0, limit = 5) =>  {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
@@ -14,11 +34,9 @@ pokeApi.getPokemons = (offset = 0, limit = 5) =>  {
     .then((jsonBoby) => jsonBoby.results)
     .then((pokemons) => pokemons.map(pokeApi.getpokemonDetalhes))
     .then((requisicaoDetalhes) => Promise.all(requisicaoDetalhes))
-    .then((pokemonsDetalhes) => {
-    debugger
-     console.log(pokemonsDetalhes)
+    .then((pokemonsDetalhes) => pokemonsDetalhes)
     
-    })
+    
     //.catch((error) => console.log(error))  // Tratamento de error, mostra o erro no console
     
 }
